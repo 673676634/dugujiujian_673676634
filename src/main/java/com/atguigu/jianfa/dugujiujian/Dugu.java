1f9a78050586af9568fce531518a4a4ebc29cfe0
qifenkaonuli <qifenkaonuli@outlook.com>
2020-06-02 14:13:40 +0800

    qifenkaonuli修改

--- src/main/java/com/atguigu/jianfa/dugujiujian/Dugu.java
+++ src/main/java/com/atguigu/jianfa/dugujiujian/Dugu.java
@@ -10,5 +10,10 @@ package com.atguigu.jianfa.dugujiujian;
 public class Dugu {
     public static void main(String[] args) {
         System.out.println("jianfa dugujianfa!!!!!!!!!!!!!!!");
+        System.out.println("jianfa dugujianfa!!!!!!!!!!!!!!!");
+        System.out.println("jianfa dugujianfa!!!!!!!!!!!!!!!");
+        System.out.println("jianfa dugujianfa!!!!!!!!!!!!!!!");
+        System.out.println("jianfa dugujianfa!!!!!!!!!!!!!!!");
+        System.out.println("jianfa dugujianfa!!!!!!!!!!!!!!!");
     }
 }
