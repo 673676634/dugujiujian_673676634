0c60fdd35e4425a02ee26b916903b51471a68a09
673676634 <673676634@qq.com>
2020-06-02 16:09:15 +0800

    新剑法

---
+++ src/main/java/com/atguigu/jianfa/dugujiujian/Newjianfa.java
@@ -0,0 +1,14 @@
+package com.atguigu.jianfa.dugujiujian;
+
+/**
+ * @Title: dugujiujian_673676634
+ * @Description:
+ * @Author: guihui
+ * @Date: 2020/6/2 16:06
+ * @Version: 1.0
+ */
+public class Newjianfa {
+    public static void main(String[] args) {
+        System.out.println("新剑法");
+    }
+}
