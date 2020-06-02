c112ddabc7c79d910debeac78de8cc0530409f13
673676634 <673676634@qq.com>
2020-06-02 14:01:44 +0800

    673676634我来提交代码，不要全局用户和密码

---
+++ src/main/java/com/atguigu/jianfa/dugujiujian/Dugu.java
@@ -0,0 +1,14 @@
+package com.atguigu.jianfa.dugujiujian;
+
+/**
+ * @Title: dugujiujian_673676634
+ * @Description:
+ * @Author: guihui
+ * @Date: 2020/6/2 13:37
+ * @Version: 1.0
+ */
+public class Dugu {
+    public static void main(String[] args) {
+        System.out.println("jianfa dugujianfa!!!!!!!!!!!!!!!");
+    }
+}
